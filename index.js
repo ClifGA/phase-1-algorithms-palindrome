b9d61780a49cab1ca92f36429aa3468890878896
Clifton <clifga@gmail.com>
2023-03-26 16:50:34 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here 
+  return word.split('').join('') === word.split('').reverse().join('') ? true : false
 }
 
 /* 
   Add your pseudocode here
+  input must equal the the reverse word
 */
 
 /*
   Add written explanation of your solution here
+  car !== rac
+  mom === mom
+  take the string reverse it check to see if it equals the input
 */
 
 // You can run `node index.js` to view these console logs
